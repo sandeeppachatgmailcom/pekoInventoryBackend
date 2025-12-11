8b302419962c09bd552d3466726e133084f33a8b
sandeep pachat <sandeeppachat@gmail.com>
2025-12-11 14:01:30 +0530

    db details added to env

--- src/database/ConnectMySql.js
+++ src/database/ConnectMySql.js
@@ -1,16 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const MySqlDb = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '[1Dr007]',
-  database: 'warehouse_db'
-}).promise();
-
-MySqlDb.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
+const MySqlDb = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
-module.exports = MySqlDb
-
+module.exports = MySqlDb;
